a7933320c3917da00f708dc4c6dc285ba4dc3269
Daniel Garcia <demonpogp@gmail.com>
2020-06-19 11:00:14 -0500

    Corregido warnings de estilo

--- CodingStandardsSI2_resuelto/src/main/java/labcodingstandards/Calculator.java
+++ CodingStandardsSI2_resuelto/src/main/java/labcodingstandards/Calculator.java
@@ -10,70 +10,63 @@ import java.util.Scanner;
  * @author
  *
  */
-public class Calculator {	
 //CHECKSTYLE: OFF
-	public static void main(String[] args) 
+public class Calculator {
+	public static void main(String[] args) {
 //CHECKSTYLE: ON
-	{
 		Scanner reader = new Scanner(System.in);
-		
         System.out.print("1. +\n2. -\n3. *\n4. /\nEnter an operator: ");
-        
         char operator = reader.nextLine().charAt(0);
-        double First;
+        double first;
         double second;
         String input;
-        
         while (true) {
         	System.out.print("Enter first number: ");
         	input = reader.nextLine();
-        	
             try {
-            	First=Integer.parseInt(input);
+            	first = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
             }
         }
-        
         while (true) {
         	System.out.print("Enter second number: ");
         	input = reader.nextLine();
-        	
             try {
-            	second=Integer.parseInt(input);
+            	second = Integer.parseInt(input);
                 break;
             } catch (NumberFormatException e) {
                 System.out.println("Not valid!");
             }
         }
 
-        Calculator cal=new Calculator();
-        String result=cal.Operation(First,second,operator);
+        Calculator cal = new Calculator();
+        String result = cal.operation(first, second, operator);
 
         System.out.printf(result);
 		reader.close();
 	}
 
-	private String Operation(double first,double second,char operator) {
+	private String operation(final double first,
+			final double second, final char operator) {
 		double result = 0;
-		switch(operator)
-        {
+		switch (operator) {
             case '1':
-                result=first+second;
+                result = first + second;
                 break;
             case '2':
-                result=first-second;
+                result = first - second;
                 break;
             case '3':
-                result=first*second;
+                result = first * second;
                 break;
             case '4':
-                result =first/second;
+                result  = first / second;
                 break;
             default:
             	return "Error! operator is not correct";
         }
-		return "The result is: "+result;
+		return "The result is: " + result;
 	}
 }
